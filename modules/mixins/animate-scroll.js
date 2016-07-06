7fc3f9717c7d15c1b75be2a7e58e3357cdde6ac7
Zach Tratar <ztratar@Zachs-Macbook-Pro.local>
2016-07-05 17:17:07 -0700

    React scroll position calc accepts containerId param

--- modules/mixins/animate-scroll.js
+++ modules/mixins/animate-scroll.js
@@ -61,7 +61,8 @@ var __container;
 var __containerRect;
 
 
-var currentPositionY = function() {
+var currentPositionY = function(options) {
+  if (!__container && options.containerId) __container = document.getElementById(options.containerId);
   if (__container) return __container.scrollTop;
   var supportPageOffset = window.pageXOffset !== undefined;
   var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
